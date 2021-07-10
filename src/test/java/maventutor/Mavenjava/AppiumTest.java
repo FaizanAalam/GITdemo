a55b05218bbd74fb86dbb2796027fad7fec74019
FaizanAaalam <faizan.qajobs@gmail.com>
2021-07-10 16:44:35 +0530

    MY First Commit

---
+++ src/test/java/maventutor/Mavenjava/AppiumTest.java
@@ -0,0 +1,13 @@
+package maventutor.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void MobileTest() {
+
+		System.out.println("This is Appium Mobile testing");
+	}
+
+}
---
+++ src/test/java/maventutor/Mavenjava/SeleniumTest.java
@@ -0,0 +1,13 @@
+package maventutor.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void SeleniumWebTest() {
+		
+		System.out.println("this is a Selenium Web Testing");
+	}
+
+}
